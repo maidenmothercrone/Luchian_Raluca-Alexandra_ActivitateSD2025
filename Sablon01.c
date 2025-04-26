22e963dd14a4749792784602a107afeb803b4d1d
Raluca Alexandra Luchian <ralucaalexandraluchian@gmail.com>
2025-04-26 19:40:19 +0300

    laborator 2

---
+++ Sablon01.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Telefon {
+	int id;
+	int RAM;
+	char* producator;
+	float pret;
+	char serie;
+};
+
+struct Telefon initializare(int id, int ram, const char* producator, float pret, char serie) {
+	struct Telefon t;
+	t.id = id;
+	t.RAM = ram;
+	t.producator = (char*)malloc(sizeof(char) * (strlen(producator)+1));
+	strcpy_s(t.producator, strlen(producator) + 1, producator);
+	t.pret = pret;
+	t.serie = serie;
+	return t;
+}
+
+void afisare(struct Telefon t) {
+	//afisarea tuturor atributelor.
+}
+
+void modifica_Atribut(struct Telefon t) {
+	//modificarea unui atribut
+}
+
+void dezalocare(struct Telefon *t) {
+	//dezalocare campuri alocate dinamic
+}
+
+int main() {
+	struct Telefon t;
+	t = initializare(1, 256, "Samsung", 2000.5, 'A');
+
+	return 0;
+}
