1650bb073d507c82c80260243b80364cdce1ff3f
Raluca Alexandra Luchian <ralucaalexandraluchian@gmail.com>
2025-04-27 20:06:35 +0300

    completare tutorial laborator 1

--- Laborator01-functii.c
+++ Laborator01-functii.c
@@ -21,20 +21,30 @@ struct Telefon initializare(int id, int ram, const char* producator, float pret,
 }
 
 void afisare(struct Telefon t) {
-	//afisarea tuturor atributelor.
+	printf("%d. Telefonul %s seria %c are %d Gb RAM si costa %5.2f RON.\n",
+		t.id, t.producator, t.serie, t.RAM, t.pret);
 }
 
-void modifica_Atribut(struct Telefon t) {
-	//modificarea unui atribut
+void modificaPret(struct Telefon* t, float noulPret) {
+	if (noulPret > 0) {
+		t->pret = noulPret;
+	}
 }
 
 void dezalocare(struct Telefon *t) {
-	//dezalocare campuri alocate dinamic
+	if (t->producator != NULL) {
+		free(t->producator);
+		t->producator = NULL;
+		printf("Memoria pentru producator a fost eliberata.\n");
+	}
 }
 
 int main() {
 	struct Telefon t;
 	t = initializare(1, 256, "Samsung", 2000.5, 'A');
-
+	afisare(t);
+	modificaPret(&t, 1500.5);
+	afisare(t);
+	dezalocare(&t);
 	return 0;
 }
